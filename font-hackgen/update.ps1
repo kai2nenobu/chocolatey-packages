1cbdfd9f310b6ea9f92e569f53092affbe04337b
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-21 21:03:05 +0900

    Capture the beginning white spaces

--- font-hackgen/update.ps1
+++ font-hackgen/update.ps1
@@ -169,9 +169,9 @@ function global:au_SearchReplace {
         }
         ".\tools\ChocolateyInstall.ps1" = @{
           '^([$]hackgenBase\s*=).*' = "`${1} '$($Latest.Prefix)_$($Latest.Tag)'"
-          '^\s*(PackageName\s*=).*' = "`${1} '$($Latest.PackageName)'"
-          '^\s*(Url\s*=).*' = "`${1} '$($Latest.URL32)'"
-          '^\s*(Checksum\s*=).*' = "`${1} '$($Latest.Checksum32)'"
+          '^(\s*PackageName\s*=).*' = "`${1} '$($Latest.PackageName)'"
+          '^(\s*Url\s*=).*' = "`${1} '$($Latest.URL32)'"
+          '^(\s*Checksum\s*=).*' = "`${1} '$($Latest.Checksum32)'"
         }
         ".\tools\common.ps1" = @{
           'HackGen(35)?(Nerd)?(Console)?-' = $fontReplacement
