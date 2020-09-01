848bbcd96e72c0c7af622e8f1e7001a5ad93af2d
Chocolatey <chocolatey@realdimensions.net>
2020-09-01 15:48:23 +0000

    AU: 2 updated - font-hackgen mape [skip ci] https://gist.github.com/bcca7b715c28d0fad4e2364eb5340438/5c4807c61af79e24ebc81620a799f58508449746

--- font-hackgen/tools/chocolateyInstall.ps1
+++ font-hackgen/tools/chocolateyInstall.ps1
@@ -4,13 +4,13 @@
 $toolsDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . (Join-Path $toolsDir 'common.ps1')
 
-$hackgenBase = 'HackGen_v2.0.0'
+$hackgenBase = 'HackGen_v2.1.1'
 $extractDir = (Join-Path $toolsDir $hackgenBase)
 
 $packageArgs = @{
   PackageName   = 'font-hackgen'
-  Url           = 'https://github.com/yuru7/HackGen/releases/download/v2.0.0/HackGen_v2.0.0.zip'
-  Checksum      = '5ad5c40f2476cd37bf48a496da3ebc5f08c2d817ea4d672fe39c6cb93d91010a'
+  Url           = 'https://github.com/yuru7/HackGen/releases/download/v2.1.1/HackGen_v2.1.1.zip'
+  Checksum      = 'f6a901b646e434d1763490ee46b4b06eca2825b4eb7efb90ef347319a407236d'
   ChecksumType  = 'sha256'
   UnzipLocation = $toolsDir
 }
