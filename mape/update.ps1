3597c79c7f68a88a3050733fc4711f5a13667974
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-09-01 23:17:52 +0900

    Use a real info instead of fixed one

--- mape/update.ps1
+++ mape/update.ps1
@@ -1,32 +1,10 @@
 Import-Module au
 
-$fixedJson = @'
-[
-  {
-    "name": "MAPE_1.0.20.0_Windows.zip",
-    "path": "Releases/MAPE_1.0.20.0_Windows.zip",
-    "sha": "ce59f24a594fcef5d5a7affea8cbb8188c660778",
-    "size": 131,
-    "url": "https://api.github.com/repos/ipponshimeji/MAPE/contents/Releases/MAPE_1.0.20.0_Windows.zip?ref=master",
-    "html_url": "https://github.com/ipponshimeji/MAPE/blob/master/Releases/MAPE_1.0.20.0_Windows.zip",
-    "git_url": "https://api.github.com/repos/ipponshimeji/MAPE/git/blobs/ce59f24a594fcef5d5a7affea8cbb8188c660778",
-    "download_url": "https://raw.githubusercontent.com/ipponshimeji/MAPE/master/Releases/MAPE_1.0.20.0_Windows.zip",
-    "type": "file",
-    "_links": {
-      "self": "https://api.github.com/repos/ipponshimeji/MAPE/contents/Releases/MAPE_1.0.20.0_Windows.zip?ref=master",
-      "git": "https://api.github.com/repos/ipponshimeji/MAPE/git/blobs/ce59f24a594fcef5d5a7affea8cbb8188c660778",
-      "html": "https://github.com/ipponshimeji/MAPE/blob/master/Releases/MAPE_1.0.20.0_Windows.zip"
-    }
-  }
-]
-'@
-
 function global:au_GetLatest {
   ## Find a latest version from a5m2 history page
   $regex = [regex]'(?i)^mape_([.\d]+)_.*\.zip$'
-  #$releases = 'https://api.github.com/repos/ipponshimeji/MAPE/contents/Releases?ref=master'
-  #$releases_info = Invoke-RestMethod -Uri $releases
-  $releases_info = $fixedJson | ConvertFrom-Json
+  $releases = 'https://api.github.com/repos/ipponshimeji/MAPE/contents/Releases?ref=master'
+  $releases_info = Invoke-RestMethod -Uri $releases
   $latestRelease = $releases_info | Where-Object { $_.name -match $regex } `
     | Sort-Object { [System.Version]($regex.Match($_.name).Groups[1].Value) } -Descending `
     | Select-Object -First 1
