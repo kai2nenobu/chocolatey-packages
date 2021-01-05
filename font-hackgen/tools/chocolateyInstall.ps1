780689f068825136dadf9dc9d8203bedf1f070bf
Chocolatey <chocolatey@realdimensions.net>
2021-01-05 02:39:55 +0000

    AU: 1 updated - font-hackgen [skip ci] https://gist.github.com/bcca7b715c28d0fad4e2364eb5340438/f224807d6003c59dcf4cccf85f0c99560430c265

--- font-hackgen/tools/chocolateyInstall.ps1
+++ font-hackgen/tools/chocolateyInstall.ps1
@@ -10,7 +10,7 @@ $extractDir = (Join-Path $toolsDir $hackgenBase)
 $packageArgs = @{
   PackageName   = 'font-hackgen'
   Url           = 'https://github.com/yuru7/HackGen/releases/download/v2.2.3/HackGen_v2.2.3.zip'
-  Checksum      = 'ef1c945ded28b3b7a56bed5bb5aaf53276ba4026dfcb7f7a6a64455770c4389e'
+  Checksum      = '81dc942372856d7d7d0e3f8a1ecef11471e23161dce25165f0cddc01bcef62f1'
   ChecksumType  = 'sha256'
   UnzipLocation = $toolsDir
 }
