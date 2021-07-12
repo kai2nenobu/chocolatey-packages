5a5b994679ef016756bb7e54b8f22bb78f3ed973
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-07-12 22:18:01 +0900

    Add an auto update script for actionlint

---
+++ actionlint/update.ps1
@@ -0,0 +1,40 @@
+import-module au
+
+function global:au_GetLatest {
+  ## Find a latest release and extract installer URL from GitHub Releases
+  $releases = 'https://api.github.com/repos/rhysd/actionlint/releases'
+  $releases_info = Invoke-RestMethod -Uri $releases
+  foreach ($release in $releases_info) {
+    if (-not $release.prerelease) {
+      $tag = $release.tag_name
+      $version = $release.tag_name -replace "^v", ""
+      $url32 = $release.assets | Where-Object { $_.name -like "*_windows_386.zip" } | Select-Object -First 1 -Expand browser_download_url
+      $url64 = $release.assets | Where-Object { $_.name -like "*_windows_amd64.zip" } | Select-Object -First 1 -Expand browser_download_url
+      return @{
+        Version = $version
+        Tag     = $tag
+        URL32   = $url32
+        URL64   = $url64
+      }
+    }
+  }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(\s+x32:).*"     = "`${1} $($Latest.URL32)"
+      "(?i)(\s+x64:).*"     = "`${1} $($Latest.URL64)"
+      "(?i)(checksum32:).*" = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*" = "`${1} $($Latest.Checksum64)"
+    }
+    ".\actionlint.nuspec"      = @{
+      '(/rhysd/actionlint/blob/)[^/<]*'         = "`${1}$($Latest.Tag)"
+      '(/rhysd/actionlint/releases/tag/)[^/<]*' = "`${1}$($Latest.Tag)"
+    }
+  }
+}
+
+Update-Package -ChecksumFor none
