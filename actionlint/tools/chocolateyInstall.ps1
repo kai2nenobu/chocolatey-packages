8264574773b32deb55abdf282ac8b284c9b5d14f
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-07-12 22:07:42 +0900

    Add an installation script for actionlint
    Expand an embedded zip file.

---
+++ actionlint/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    PackageName    = 'actionlint'
+    FileFullPath   = Get-Item "$toolsPath\*_windows_386.zip"
+    FileFullPath64 = Get-Item "$toolsPath\*_windows_amd64.zip"
+    Destination    = $toolsPath
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
