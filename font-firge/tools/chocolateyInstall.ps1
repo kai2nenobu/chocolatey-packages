808a3f6a6cb1a0da7e7e6358b402058030c5cf1f
Chocolatey <chocolatey@realdimensions.net>
2021-07-24 08:12:19 +0000

    AU: 1 updated - font-firge [skip ci] https://gist.github.com/bcca7b715c28d0fad4e2364eb5340438/d90a3b02172da2a0db0a333d8aefd88561a52b9f

--- font-firge/tools/chocolateyInstall.ps1
+++ font-firge/tools/chocolateyInstall.ps1
@@ -4,13 +4,13 @@
 $toolsDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . (Join-Path $toolsDir 'common.ps1')
 
-$firgeBase = 'Firge_v0.0.2'
+$firgeBase = 'Firge_v0.1.0'
 $extractDir = (Join-Path $toolsDir $firgeBase)
 
 $packageArgs = @{
   PackageName   = 'font-firge'
-  Url           = 'https://github.com/yuru7/Firge/releases/download/v0.0.2/Firge_v0.0.2.zip'
-  Checksum      = '136c4b32aa862db3b6963dfdf73fb56a3aa70fe6fa37b1d304349ee3cef3d2a1'
+  Url           = 'https://github.com/yuru7/Firge/releases/download/v0.1.0/Firge_v0.1.0.zip'
+  Checksum      = 'd94ef9db6016d9dbf043b03ab45f36dd266ca930063294f464976fc09fa99489'
   ChecksumType  = 'sha256'
   UnzipLocation = $toolsDir
 }
