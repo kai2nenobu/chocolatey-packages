df9d81ff0bab8eaffe1fb944919cec1b98c91cbd
Tsunenobu Kai <kai2nenobu@gmail.com>
2022-08-13 17:34:35 +0900

    Sort font names in a natural order
    And we use OrderedDictionary to keep the order of font names.

--- font-hackgen/tools/common-font-hackgen-nerd.ps1
+++ font-hackgen/tools/common-font-hackgen-nerd.ps1
@@ -1,10 +1,10 @@
-﻿$hackgenFonts = @{
-  'HackGen35ConsoleNF-Bold.ttf' = 'HackGen35 Console NF (Bold)'
-  'HackGen35ConsoleNF-Regular.ttf' = 'HackGen35 Console NF (Regular)'
-  'HackGen35ConsoleNFJ-Bold.ttf' = 'HackGen35 Console NFJ (Bold)'
+﻿$hackgenFonts = [ordered]@{
+  'HackGenConsoleNF-Regular.ttf'    = 'HackGen Console NF (Regular)'
+  'HackGenConsoleNF-Bold.ttf'       = 'HackGen Console NF (Bold)'
+  'HackGenConsoleNFJ-Regular.ttf'   = 'HackGen Console NFJ (Regular)'
+  'HackGenConsoleNFJ-Bold.ttf'      = 'HackGen Console NFJ (Bold)'
+  'HackGen35ConsoleNF-Regular.ttf'  = 'HackGen35 Console NF (Regular)'
+  'HackGen35ConsoleNF-Bold.ttf'     = 'HackGen35 Console NF (Bold)'
   'HackGen35ConsoleNFJ-Regular.ttf' = 'HackGen35 Console NFJ (Regular)'
-  'HackGenConsoleNF-Bold.ttf' = 'HackGen Console NF (Bold)'
-  'HackGenConsoleNF-Regular.ttf' = 'HackGen Console NF (Regular)'
-  'HackGenConsoleNFJ-Bold.ttf' = 'HackGen Console NFJ (Bold)'
-  'HackGenConsoleNFJ-Regular.ttf' = 'HackGen Console NFJ (Regular)'
+  'HackGen35ConsoleNFJ-Bold.ttf'    = 'HackGen35 Console NFJ (Bold)'
 }
--- font-hackgen/tools/common-font-hackgen.ps1
+++ font-hackgen/tools/common-font-hackgen.ps1
@@ -1,10 +1,10 @@
-﻿$hackgenFonts = @{
-  'HackGen-Bold.ttf' = 'HackGen (Bold)'
-  'HackGen-Regular.ttf' = 'HackGen (Regular)'
-  'HackGen35-Bold.ttf' = 'HackGen35 (Bold)'
-  'HackGen35-Regular.ttf' = 'HackGen35 (Regular)'
-  'HackGen35Console-Bold.ttf' = 'HackGen35 Console (Bold)'
+﻿$hackgenFonts = [ordered]@{
+  'HackGen-Regular.ttf'          = 'HackGen (Regular)'
+  'HackGen-Bold.ttf'             = 'HackGen (Bold)'
+  'HackGenConsole-Regular.ttf'   = 'HackGen Console (Regular)'
+  'HackGenConsole-Bold.ttf'      = 'HackGen Console (Bold)'
+  'HackGen35-Regular.ttf'        = 'HackGen35 (Regular)'
+  'HackGen35-Bold.ttf'           = 'HackGen35 (Bold)'
   'HackGen35Console-Regular.ttf' = 'HackGen35 Console (Regular)'
-  'HackGenConsole-Bold.ttf' = 'HackGen Console (Bold)'
-  'HackGenConsole-Regular.ttf' = 'HackGen Console (Regular)'
+  'HackGen35Console-Bold.ttf'    = 'HackGen35 Console (Bold)'
 }
