494becfc99bf8695104ea69be402dad8f42633f2
Tsunenobu Kai <kai2nenobu@gmail.com>
2025-01-30 22:47:31 +0900

    Add a package for git-credential-keepassxc
    This commit introduces the foundational elements of the
    git-credential-keepassxc package, including the necessary metadata
    configuration files, a README for usage instructions, and legal
    documents like the license and verification procedures. The package
    allows integration of KeePassXC as a credential store for Git
    operations, enabling users to easily store and retrieve credentials.
    
    The addition of parameters for build types—'minimal' and 'full'—provides
    flexibility in how the binary is installed, tailored to user needs. This
    update ensures that users have clear guidance on installation,
    configuration, and licensing, contributing to a more robust software
    distribution process.

---
+++ git-credential-keepassxc/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$pp = Get-PackageParameters
+if (!$pp['BuildType']) {
+    $pp['BuildType'] = 'minimal'
+}
+switch($pp['BuildType']) {
+    'minimal' { $zipPath = 'windows-latest-minimal.zip' }
+    'full' { $zipPath = 'windows-latest-full.zip' }
+    default { throw "'BuildType' parameter is invalid: $($pp['BuildType'])" }
+}
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    PackageName    = 'git-credential-keepassxc'
+    FileFullPath   = "$toolsPath\$zipPath"
+    Destination    = $toolsPath
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
